43a19d37f24d5a0500ae25b612ccb3f598a5d653
Seunghyun <mona1222@naver.com>
2026-07-14 09:35:19 +0900

    Feat: Update Calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,18 @@
-function add(a, b) {
+const add = (a, b) => {
   return a + b;
-}
+};
 
-function subtract(a, b) {
-  return a-b;
-}
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiplay = (a, b) => {
+  return a * b;
+};
+
+const add = (a, b) => {
+  if (b === 0) {
+    throw new Error();
+  }
+  return a / b;
+};
